6a7f15ec7ec6adce5ca7c9248945fce6887223de
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-24 01:58:56 +0530

    update

--- C++/2Dvector.cpp
+++ C++/2Dvector.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {   
 	int n,a;
+	cout<<"enter the size of array :";
 	cin>>n;
 vector<int> d;
+cout<<"enter the elemant of array :";
 for(int i=0;i<n;i++)
 {
   cin>>a;
